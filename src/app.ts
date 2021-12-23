09ce85fceca87fa16b033e402da0910957d436b7
GlebRaychenkov <raychenkovgleb@gmail.com>
2021-12-23 16:12:07 +0300

    initial commit

---
+++ src/app.ts
@@ -0,0 +1,29 @@
+import express, { Express } from 'express'
+import { Server } from 'http'
+import { LoggerService } from './logger/logger.service'
+import { UserController } from './users/users.controller';
+
+export class App {
+	app: Express;
+	server: Server;
+	port: number;
+	logger: LoggerService;
+	userController: UserController
+
+	constructor(port: number, logger: LoggerService, userController: UserController) {
+		this.app = express();
+		this.port = port;
+		this.logger = logger;
+		this.userController = userController
+	}
+
+	useRoutes() {
+		this.app.use('/users', this.userController.router);
+	}
+
+	public async init() {
+		this.useRoutes();
+		this.server = this.app.listen(this.port);
+		this.logger.log(`Server started on ${this.port}`);
+	}
+}
